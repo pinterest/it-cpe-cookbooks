8efd99f80fab59d854a38521ea9778301b84539b
Austin Culter <austin.culter@gusto.com>
2018-08-31 15:36:45 -0600

    Init. net-new Windows recipe.

---
+++ cpe_firewall/recipes/windows.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: cpe_firewall
+# Recipe:: windows
+#
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+#
+# Copyright (c) 2017-present, Pinterest, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+return unless node.windows?
+
+windows_service 'firewall' do
+  service_name 'MpsSvc'
+  run_as_user 'NT AUTHORITY\LocalService'
+  startup_type :automatic
+  action :start
+end
