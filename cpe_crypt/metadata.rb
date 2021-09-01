ad2b484f8a411b2aadc2bae1127e9d592e69e8a8
Austin Culter <austinculter@gmail.com>
2021-09-01 14:41:59 +0200

    Update metadata.rb

--- cpe_crypt/metadata.rb
+++ cpe_crypt/metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'itcpe@pinterest.com'
 license 'Apache-2.0'
 description 'Installs/Configures cpe_crypt'
 version '0.1.0'
-chef_version '>= 12.6'
+chef_version '>= 14.14'
 supports 'mac_os_x'
 
 depends 'cpe_launchd'
