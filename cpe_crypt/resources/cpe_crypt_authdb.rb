1ecd4daf6936bc76c07bea23735ebc2ea38c2c95
Austin Culter <austinculter@gmail.com>
2021-09-01 14:42:02 +0200

    Update cpe_crypt_authdb.rb

--- cpe_crypt/resources/cpe_crypt_authdb.rb
+++ cpe_crypt/resources/cpe_crypt_authdb.rb
@@ -10,6 +10,7 @@
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 #
+unified_mode true
 
 resource_name :cpe_crypt_authdb
 provides :cpe_crypt_authdb, :os => 'darwin'
